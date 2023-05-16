288697634049968008be0d53532377780a6ac0cc
andyromerodev <andy910304@gmail.com>
2023-05-16 12:13:31 -0400

    4. Kotlin Coroutines - runBlocking

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,12 +1,22 @@
 import kotlinx.coroutines.*
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+
+val formatter = DateTimeFormatter.ISO_LOCAL_TIME
+val time = { formatter.format(LocalDateTime.now()) }
+
+suspend fun getValue(): Double {
+    println("entering getValue() at ${time()}")
+    delay(3000)
+    println("leaving getValue() at ${time()}")
+    return Math.random()
+}
 
 fun main() {
-
-    println("Hola Andy")
-
-    repeat(3) {
-        GlobalScope.launch {
-            println("Hi from ${Thread.currentThread()}")
-        }
+    runBlocking {
+        val num1 = async { getValue() }
+        val num2 = async { getValue() }
+        println("result of num1 + num2 is")
+        println("result of num1 + num2 is ${num1.await() + num2.await()}")
     }
 }
